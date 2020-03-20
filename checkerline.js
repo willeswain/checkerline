6556d3d1065c58fe1e28dfbc1802d0d6dcafcd9e
Jon Swain <jonswain@JonsMacBookPro-2.local>
2020-03-20 13:56:29 -0700

    linked everything

--- checkerline.js
+++ checkerline.js
@@ -1,10 +1,10 @@
-let button = document.getElementById("reset");
 
 var box1 = document.getElementById("box1");
 var box2 = document.getElementById("box2");
 var box3 = document.getElementById("box3");
 var box4 = document.getElementById("box4");
 var box5 = document.getElementById("box5");
+var button = document.getElementById("reset");
 
 box1.addEventListener("mouseover", event => {
   changeColor(event);
